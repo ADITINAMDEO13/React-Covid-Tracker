3fb4c2734c7a58232aa73b726e03dfa9f82efc9e
Namdev <adnamdev@deloitte.com>
2021-04-17 10:29:42 +0530

    first commit

---
+++ src/LineGraph.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import numeral from "numeral";
+
+
+const LineGraph = () => {
+  return <h4></h4>
+}
+export default LineGraph;
+
